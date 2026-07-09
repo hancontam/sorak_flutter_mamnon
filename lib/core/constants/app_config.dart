5aa9b5ded4ac05e8744d0621fa271ac2f1a6585f
Han Nguyen <hancontam@gmail.com>
2026-07-09 12:14:26 +0700

    Allow API mode configuration via dart define

--- lib/core/constants/app_config.dart
+++ lib/core/constants/app_config.dart
@@ -1,6 +1,13 @@
 class AppConfig {
   const AppConfig._();
 
-  static const bool useMockApi = true;
-  static const String apiBaseUrl = 'http://localhost:3000/api';
+  static const bool useMockApi = bool.fromEnvironment(
+    'USE_MOCK_API',
+    defaultValue: true,
+  );
+
+  static const String apiBaseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'http://localhost:3000/api',
+  );
 }
